7c7b793df963644dfbd93a0c7cfcb4cff1bca8cd
Zoe <doticus@gmail.com>
2014-11-25 23:40:24 -0500

    Started project
    To do:
    1) connect links
    2) Fix CSS
    3) Figure out how to index/search things

---
+++ server.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'CSV'
+
+def data(filename)
+  @data_file = []
+
+  CSV.foreach(filename, headers: true) do |row|
+    @data_file << {
+      id: row['id'],
+      author: row['author'],
+      drink: row['drink'],
+      drinker: row['drinker'],
+    }
+  end
+
+  @data_file
+end
+
+get '/home' do
+
+@data = data("data.csv")
+
+  erb :index
+end
