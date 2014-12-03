51d93fce01adb43ad18fcabcb37aeb0fa78943e6
Zoe <doticus@gmail.com>
2014-12-03 17:15:52 -0500

    Sort of working, but not really.
    Pulling sentences that have no booze.  But some are pretty good sentences.q

--- server.rb
+++ server.rb
@@ -4,5 +4,72 @@ require 'pry'
 
 get '/home' do
 
+  def get_booze(file)
+    @list_of_drinks = []
+    initial_list = File.read(file).split("\n")
+    @list_of_drinks = initial_list.uniq!
+  end
+
+  def get_novel(name, author, file)
+    @name = name
+    @author = author
+    @sentences = []
+    original_text = File.read(file)
+    @sentences = original_text.split(/[.!?:]/)
+  end
+
+  def find_booze
+    @has_booze = []
+    @list_of_drinks.each do |booze|
+      @sentences.each do |i|
+        if i.downcase.include?(booze)
+          @has_booze << i
+        end
+      end
+    end
+  end
+
+  def print_booze
+    @has_booze.each do |i|
+      puts i
+    end
+  end
+
   erb :index
+end
+
+get "/test" do
+
+  def get_booze(file)
+    @list_of_drinks = []
+    initial_list = File.read(file).split("\n")
+    @list_of_drinks = initial_list.uniq!
+  end
+
+  def get_novel(name, author, file)
+    @name = name
+    @author = author
+    @sentences = []
+    original_text = File.read(file)
+    @sentences = original_text.split(/[.!?:]/)
+  end
+
+  def find_booze
+    @has_booze = []
+    @list_of_drinks.each do |booze|
+      @sentences.each do |i|
+        if i.downcase.include?(booze)
+          @has_booze << i
+        end
+      end
+    end
+  end
+
+  def print_booze
+    @has_booze.each do |i|
+      puts i
+    end
+  end
+
+erb :test
 end
