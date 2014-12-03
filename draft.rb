b393d7d88a5c6760d780b53b374416cc70c1c801
Zoe <doticus@gmail.com>
2014-12-03 15:17:28 -0500

    It is starting to work! Time to look at the output

--- draft.rb
+++ draft.rb
@@ -6,30 +6,50 @@ def get_booze(file)
   @list_of_drinks = initial_list.uniq!
 end
 
-def get_novel(file)
+def get_novel(file) #returns an array of sentences
   @sentences = []
-  @words = []
    original_text = File.read(file)
-   @sentences = original_text.split(".")
-   @words = original_text.split(" ")
+   @sentences = original_text.split(/[.!?:]/)
 end
 
 def find_booze
-  @list_of_drinks.each do |drink|
-    if @words.any?{|word| word == drink}
-      puts "#{drink}"
+  @has_booze = []
+  @list_of_drinks.each do |booze|
+    @sentences.each do |i|
+    if i.downcase.include?(booze)
+      @has_booze << i
     end
+  end
   end
 end
 
-get_booze('alcohol.txt')
-get_novel('bovary.txt')
-find_booze
+def print_booze
+  @has_booze.each do |i|
+    puts i
+  end
+end
 
-get_booze('alcohol.txt')
-get_novel('mymanjeeves.txt')
-find_booze
+# get_booze('alcohol.txt')
+# get_novel('burgundytestfile.txt')
+# find_booze
+# puts "#{@has_booze}"
+
+# get_booze('alcohol.txt')
+# get_novel('bovary.txt')
+# find_booze
+# print_booze
+# #
+# get_booze('alcohol.txt')
+# get_novel('mymanjeeves.txt')
+# find_booze
+# print_booze
 
 get_booze('alcohol.txt')
 get_novel('pierreetjean.txt')
 find_booze
+print_booze
+# #
+# get_booze('alcohol.txt')
+# get_novel('shelley_frankie.txt')
+# find_booze
+# puts "#{@has_booze}"
--- server.rb
+++ server.rb
@@ -2,24 +2,7 @@ require 'sinatra'
 require 'CSV'
 require 'pry'
 
-def data(filename)
-  @data_file = []
-
-  CSV.foreach(filename, headers: true) do |row|
-    @data_file << {
-      id: row['id'],
-      author: row['author'],
-      drink: row['drink'],
-      drinker: row['drinker'],
-    }
-  end
-
-  @data_file
-end
-
 get '/home' do
-
-@data = data("data.csv")
 
   erb :index
 end
